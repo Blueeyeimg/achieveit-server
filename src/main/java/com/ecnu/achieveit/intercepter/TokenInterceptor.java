7e50883503693617e8a7bff545c6f3f1d38f2c12
nishitong <10165101281@stu.ecnu.edu.cn>
2020-03-13 23:38:22 +0800

    修改token拦截器，使其支持两种token认证方式 1. 在http header中添加Authorization : Bearer <token> 2. 在http parameter中添加token ： <token>

--- src/main/java/com/ecnu/achieveit/intercepter/TokenInterceptor.java
+++ src/main/java/com/ecnu/achieveit/intercepter/TokenInterceptor.java
@@ -2,6 +2,8 @@ package com.ecnu.achieveit.intercepter;
 
 import com.ecnu.achieveit.util.JwtConfig;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -24,16 +26,21 @@ public class TokenInterceptor extends HandlerInterceptorAdapter {
             return true ;
         }
         // Token 验证
-        String token = request.getHeader(jwtConfig.getHeader());
+        String token = request.getHeader("Authorization");
+
         if(StringUtils.isEmpty(token)){
             token = request.getParameter(jwtConfig.getHeader());
+        }else if(token.startsWith("Bearer")){
+            token = token.substring(7);
+        }else{
+            throw new Exception("Authorization" + "不以Bearer开头。");
         }
         if(StringUtils.isEmpty(token)){
-            throw new Exception(jwtConfig.getHeader()+ "不能为空");
+            throw new Exception(jwtConfig.getHeader()+ "不能为空。");
         }
         Claims claims = jwtConfig.getTokenClaim(token);
         if(claims == null || jwtConfig.isTokenExpired(claims.getExpiration())){
-            throw new Exception(jwtConfig.getHeader() + "失效，请重新登录");
+            throw new Exception(jwtConfig.getHeader() + "失效，请重新登录。");
         }
         //设置 identityId 用户身份ID
         request.setAttribute("identityId", claims.getSubject());
