46532da7267858652a47a9c15d00565de26b062d
the little one <ywan.ecnu@qq.com>
2020-03-23 01:14:07 +0800

    Revert "项目基本信息查询"
    This reverts commit d2bf6894fe13c6162fd060c93746cf19de3f5db6.

--- src/main/java/com/ecnu/achieveit/service/ProjectService.java
+++ src/main/java/com/ecnu/achieveit/service/ProjectService.java
@@ -2,21 +2,9 @@ package com.ecnu.achieveit.service;
 
 import com.ecnu.achieveit.model.ProjectBasicInfo;
 
-import java.util.List;
-
 public interface ProjectService {
     boolean addProject(ProjectBasicInfo projectBasicInfo);
 
     boolean updateProject(ProjectBasicInfo projectBasicInfo);
-
-    ProjectBasicInfo querryProjectByPrimaryKey(String projectId);
-
-    List<ProjectBasicInfo> querryProjectByEmployeeId(String employeeId);
-
-    List<ProjectBasicInfo> querryProjectByClientId(String clientId);
-
-    List<ProjectBasicInfo> querryProjectByState(String state);
-
-    List<ProjectBasicInfo> querryProjectByBossId(String BossId);
 
 }
