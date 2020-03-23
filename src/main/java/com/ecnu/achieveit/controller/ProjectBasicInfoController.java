499049c694ffb9aa06a362589aa6b8c808f5e0a7
the little one <ywan.ecnu@qq.com>
2020-03-23 13:41:08 +0800

    若数据库未查到结果返回错误提示
    由于查询的返回值是一个List，未查到结果返回的是一个空的List，值为[]，不能直接用==null去判断，而应该用该list的.size()==0去判断

--- src/main/java/com/ecnu/achieveit/controller/ProjectBasicInfoController.java
+++ src/main/java/com/ecnu/achieveit/controller/ProjectBasicInfoController.java
@@ -20,7 +20,7 @@ public class ProjectBasicInfoController {
 
     @GetMapping("/project_info/{project_id}")
     public Object show(@PathVariable("project_id")String projectId){
-        Object result = projectService.querryProjectByPrimaryKey(projectId);
+        ProjectBasicInfo result = projectService.querryProjectByPrimaryKey(projectId);
         if(result == null) return RestResponse.fail();
         return RestResponse.success(result);
     }
@@ -29,23 +29,23 @@ public class ProjectBasicInfoController {
     public Object list1(@PathVariable("item") String item,
                         @PathVariable("item_value")String itemValue){
         if(item.equals("employee_id")){
-            Object result = projectService.querryProjectByEmployeeId(itemValue);
-            if(result == null) return RestResponse.fail(); //"未查到"+itemValue+"有关信息"
+            List<ProjectBasicInfo> result = projectService.querryProjectByEmployeeId(itemValue);
+            if(result.size() == 0) return RestResponse.fail("未查到"+itemValue+"有关信息");
             else return RestResponse.success(result);
         }
         else if(item.equals("client_id")){
-            Object result = projectService.querryProjectByClientId(itemValue);
-            if(result == null) return RestResponse.fail();//"未查到"+itemValue+"有关信息"
+            List<ProjectBasicInfo> result = projectService.querryProjectByClientId(itemValue);
+            if(result.size() == 0) return RestResponse.fail("未查到"+itemValue+"有关信息");
             else return RestResponse.success(result);
         }
         else if(item.equals("state")) {
-            Object result = projectService.querryProjectByState(itemValue);
-            if(result.equals(0)) return RestResponse.fail();//"未查到"+itemValue+"有关信息"
+            List<ProjectBasicInfo> result = projectService.querryProjectByState(itemValue);
+            if(result.size() == 0) return RestResponse.fail("未查到"+itemValue+"有关信息");
             else return RestResponse.success(result);
         }
         else if(item.equals("boss_id")){
-            Object result = projectService.querryProjectByBossId(itemValue);
-            if(result.equals(0)) return RestResponse.fail();//"未查到"+itemValue+"有关信息"
+            List<ProjectBasicInfo> result = projectService.querryProjectByBossId(itemValue);
+            if(result.size() == 0) return RestResponse.fail("未查到"+itemValue+"有关信息");
             else return RestResponse.success(result);
         }
         return RestResponse.fail();
